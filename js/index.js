2835bd6dd29e7c015f7485011c3e22f9b2a24171
zhipeng.huo <546879387@qq.com>
2018-05-15 10:41:01 +0800

    index done 2018.5.15

--- js/index.js
+++ js/index.js
@@ -1,31 +1,47 @@
-$(window).scroll(function () {
-    let offsetTop = $(".navbar").offset().top;
-    if (offsetTop >= 48) {
-        $(".fixed-top").addClass("top-nav");
-    } else {
-        $(".fixed-top").removeClass("top-nav");
-    }
-})
+// //五星好评
+// var stars = document.getElementsByClassName("star");
+// var flag = 5; //这个值随便取，只要不是01234就行
 
-//移动端滑动轮播图
-var carousels = $(".carousel"); //获取所有的轮播图
-var startX, endX, finalMove;
-var diviation = 60; //让手指滑动一定距离轮播图才工作
-carousels.on("touchstart", function (event) {
-    // console.log(event.originalEvent.touches[0].pageX);
-    startX = event.originalEvent.touches[0].pageX; //获取手指接触屏幕时的位置
-});
-carousels.on("touchmove", function (event) {
-    // console.log(event.originalEvent.touches[0].pageX);
-    endX = event.originalEvent.touches[0].pageX; //手指滑动时该值一直刷新，当手指离开时保留最后一次手指的位置
-})
-carousels.on("touchend", function (event) {
-            //console.log(event.originalEvent.touches[0].pageX);
-            finalMove = Math.abs(startX - endX) - diviation;
-            if (finalMove > 0 && (startX - endX) > 0) { //如果手指滑动方向向左，轮播图向右播放一张图片
-                $(this).carousel('next');
-                }
-                else if (finalMove > 0 && (startX - endX) < 0) {
-                    $(this).carousel('prev');
-                }
-            })
+// //循环载入鼠标移入事件
+// for (var i = 0; i < stars.length; i++) {　　　　
+//     stars[i].onmouseover = function () {　　　　　　
+//         var id = parseInt(this.id);　　　　　　
+//         for (var i = 0; i <= id; i++) {　　　　　　　　
+//             stars[i].innerHTML = "★";　　　　　　
+//         }　　　　　　
+//         for (var j = id + 1; j < 5; j++) {　　　　　　　　
+//             stars[j].innerHTML = "☆";　　　　　　
+//         }　　　　
+//     };　　
+// }　　 
+
+// //循环载入鼠标点击星星事件
+// for (var i = 0; i < stars.length; i++) {　　　　
+//     stars[i].onclick = function () {　　　　　　
+//         var id = parseInt(this.id);　　　　　　
+//         flag = id;　　　　　　
+//         for (var i = 0; i <= id; i++) {　　　　　　　　
+//             stars[i].innerHTML = "★";　　　　　　
+//         }　　　　　　
+//         //Expand(); //这里是鼠标点击星星的扩展，例如出现分值之类的，总之要扩展什么随你的大小便啦~
+//         　　　　
+//     };　　
+// }　　 
+
+// //载入鼠标离开div事件
+// document.getElementById("stars").onmouseout = function () {　　　　 //如果tag是3，则循环给把0 1 2 3几个星星整黄
+//     if (flag >= 0 && flag <= 4) {　　　　　　
+//         for (var i = 0; i <= flag; i++) {　　　　　　　　
+//             stars[i].innerHTML = "★";　　　　　　
+//         }　　　　　　
+//         for (var j = flag + 1; j < 5; j++) {　　　　　　　　
+//             stars[j].innerHTML = "☆";　　　　　
+//         }　　
+//     }　　　　 //如果tag没有值或者是初值5，则把所有的星星还原成空星星
+//     　　　　
+//     else {　　　　　　
+//         for (var i = 0; i < stars.length; i++) {　　　　　　　　
+//             stars[i].innerHTML = "☆";　　　　　　
+//         }　　　　
+//     }　　
+// };
